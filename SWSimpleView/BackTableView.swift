ca4047353a896abf7ffbaa4cc509e14796bbf783
Kumar <kumar@users-MacBook-Pro.local>
2017-03-07 12:42:52 +0530

    Populates the table view on my left side with data.

---
+++ SWSimpleView/BackTableView.swift
@@ -0,0 +1,38 @@
+//
+//  BackTableView.swift
+//  SWSimpleView
+//
+//  Created by Kumar on 07/03/17.
+//  Copyright © 2017 Workout. All rights reserved.
+//
+
+import Foundation
+
+class BackTableVC: UITableViewController {
+    
+    var TableArray = [String]()
+    
+    override func viewDidLoad() {
+        TableArray = ["Hello", "Second", "World"];
+        
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return TableArray.count;
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = TableArray[indexPath.row];
+        
+        
+        return cell;
+        
+    }
+    
+}
